a4009ac121039fa18e463fc76f32e6d69bf7ae66
redouane dahmani <redouanedahmani@hotmail.fr>
2020-02-02 22:55:21 +0100

    Redouane 2eme commit

--- 00_Projet_Gestion_Ecole/src/com/intiformation/gestionecole/test/AppTestAdministrateur.java
+++ 00_Projet_Gestion_Ecole/src/com/intiformation/gestionecole/test/AppTestAdministrateur.java
@@ -10,17 +10,9 @@ public class AppTestAdministrateur {
 		/* =================== Ajouter Administrateur =================== */
 		/* ============================================================== */
 
-		/* =============== Relation unidirectionnelle =================== */
-		/**
-		 * 1. Récupérer l'adresse de la personne à l'id = 1
-		 *  -> utilisation de l'entité Administrateur
-		 */
 		// administrateur à ajouter
 		Administrateur administrateur = new Administrateur("mdp123", "AdminNom", "AdminPrenom", "admin@admin.com");
-//		// adresse à ajouter
-//		Adresse adresse = new Adresse("15 rue de chez moi", 33000, "Bordeaux");
-//		// 3. Association de l'administrateur à l'adresse
-//		administrateur.setAdresse(adresse);
+
 		// DAO Administrateur
 		AdministrateurDaoImpl administrateurDao = new AdministrateurDaoImpl();
 		// Ajout administrateur
@@ -43,7 +35,6 @@ public class AppTestAdministrateur {
 		System.out.println("\t > Mot de passe : " + administrateurDao.getById(1).getMotDePasse());
 		System.out.println("\t > Nom : " + administrateurDao.getById(1).getNom());
 		System.out.println("\t > Prenom : " + administrateurDao.getById(1).getPrenom());
-	//	System.out.println("\t > Adresse : " + administrateurDao.getById(1).getAdresse());
 		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n");
 
 		System.out.println("Get by id toString ++++++++++++++++++++++++++++++++++++++++++++++++++");
